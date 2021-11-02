7707e353d4286fec48c2ed85736695e373ee4651
이인환 <ihlee1237@gmail.com>
2021-11-02 23:01:18 +0900

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/header/index";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Header />
         <Route path="/post/:id"></Route>
         <Route path="/"></Route>
       </Switch>
