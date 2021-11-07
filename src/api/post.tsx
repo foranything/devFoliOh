e8528174160224bfbe77fbe1cb5201a345d236fa
lotus9630 <lotus9630@gmail.com>
2021-11-08 01:28:09 +0900

    삭제 기능 추가

--- src/api/post.tsx
+++ src/api/post.tsx
@@ -1,22 +1,40 @@
 import axios from "axios";
-import { Post, Response } from "../types";
+// import { Post, Response } from "../types";
 
 const server = "https://limitless-sierra-67996.herokuapp.com/v1";
 
 const get = async (uri: string): Promise<any> => {
-  try {
-    return await axios.get(server + uri);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        return await axios.get(server + uri);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-export const getPost = async (id: string): Promise<Post> => {
-  const response = await get(`/posts/${id}`);
-  return response.data;
+export const getPost = async (id: string): Promise<any> => {
+    try {
+        const response = await get(`/posts/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-export const getAllPosts = async (): Promise<Response<Post[]>> => {
-  const response = await get("/posts");
-  return response.data;
+export const getAllPosts = async (): Promise<any> => {
+    try {
+        const response = await get("/posts");
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deletePost = async (id: string): Promise<any> => {
+    try {
+        const response = await axios.delete(`${server}/posts/${id}`);
+        console.log("check");
+        return response.status;
+    } catch (error) {
+        console.log(error);
+    }
 };
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,61 +1,77 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import styled from "styled-components";
-import { getPost } from "../api/post";
+import { getPost, deletePost } from "../api/post";
 import { Post } from "../types";
+import Modal from "../components/modal";
 
 const Container = styled.div`
-  width: 768px;
-  margin: 0 auto;
-  margin-top: 150px;
-  .title {
-    font-size: 48px;
-    margin-bottom: 32px;
-  }
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 150px;
+    .title {
+        font-size: 48px;
+        margin-bottom: 32px;
+    }
 `;
 
 const ButtonList = styled.section`
-  display: flex;
-  justify-content: flex-end;
-  span {
-    margin-left: 20px;
-    margin-bottom: 40px;
-  }
+    display: flex;
+    justify-content: flex-end;
+    span {
+        margin-left: 20px;
+        margin-bottom: 40px;
+        cursor: pointer;
+    }
 `;
 
 const TagList = styled.section`
-  span {
-    color: green;
-    background-color: rgb(241, 243, 245);
-    margin-right: 20px;
-    border-radius: 1rem;
-  }
-  margin-bottom: 30px;
+    span {
+        color: green;
+        background-color: rgb(241, 243, 245);
+        margin-right: 20px;
+        border-radius: 1rem;
+    }
+    margin-bottom: 30px;
 `;
 
 export default function PostComponent() {
-  const { id }: any = useParams();
-  const [post, setPost] = useState({} as Post);
-  useEffect(() => {
-    (async function () {
-      const data = await getPost(id);
-      setPost(data);
-    })();
-  }, [id]);
+    const [onModal, setOnModal] = useState(false);
+    const { id }: any = useParams();
+    const [post, setPost] = useState({} as Post);
+    useEffect(() => {
+        (async function () {
+            const data = await getPost(id);
+            setPost(data);
+        })();
+    }, [id]);
 
-  return (
-    <Container>
-      <div className="title">{post.title}</div>
-      <ButtonList>
-        <span>글 수정</span>
-        <span>글 삭제</span>
-      </ButtonList>
-      <TagList>
-        {post.tags?.map((tag) => (
-          <span key={tag}>tag</span>
-        ))}
-      </TagList>
-      <article className="body">{post.body}</article>
-    </Container>
-  );
+    const toggleModal = (toggle: boolean) => {
+        setOnModal(toggle);
+    };
+
+    return (
+        <Container>
+            {onModal ? (
+                <Modal
+                    toggleModal={toggleModal}
+                    deletePost={deletePost}
+                    id={id}
+                />
+            ) : (
+                ""
+            )}
+            <div className="title">{post.title}</div>
+            <ButtonList>
+                <span>글 수정</span>
+                <span onClick={() => toggleModal(true)}>글 삭제</span>
+            </ButtonList>
+            <TagList>
+                {post.tags?.map((tag) => (
+                    <span key={tag}>tag</span>
+                ))}
+            </TagList>
+            <article className="body">{post.body}</article>
+        </Container>
+    );
 }
